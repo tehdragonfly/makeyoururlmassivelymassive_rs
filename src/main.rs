995030303355fa30ff7fc2f4247fdffd07de2a72
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-12-03 23:43:38 +0000

    Use a template.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 
 #[macro_use] extern crate rocket;
 
+use std::collections::HashMap;
+use rocket_contrib::templates::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("index", context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
