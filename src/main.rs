b729be65913f0469d1916d84541c6dab978a5f6b
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-12-05 23:24:29 +0000

    Add a database.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 
 use std::collections::HashMap;
+
+use rocket_contrib::databases::diesel;
 use rocket_contrib::templates::Template;
+
+
+#[database("default")]
+struct DefaultDatabase(diesel::PgConnection);
+
 
 #[get("/")]
 fn index() -> Template {
@@ -11,9 +19,18 @@ fn index() -> Template {
     Template::render("index", context)
 }
 
+
+#[get("/<path>")]
+fn redirect(conn: DefaultDatabase, path: String) -> String {
+    path
+}
+
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
+        .mount("/", routes![redirect])
+        .attach(DefaultDatabase::fairing())
         .attach(Template::fairing())
         .launch();
 }
